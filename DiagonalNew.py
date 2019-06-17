93f7b05b8a42fbb03e86eaab6c1b9e7bb949b9d1
Aleks Czejdo <31680728+aczejdo@users.noreply.github.com>
2019-06-17 09:18:22 -0400

    Add files via upload

--- DiagonalNew.py
+++ DiagonalNew.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 from copy import deepcopy
 import matplotlib.cm as cm
 from functools import partial
-
+import vapeplot as vapeplot
 #-------------Generate Fock Basis, Stackexchange++-------------------------------
 def generate(N,nb):
         states = np.zeros((int(comb(nb+N-1, nb)), N), dtype=int)
@@ -243,7 +243,7 @@ def getPxyParr(p):
     M=np.zeros((L//nd,L//nd))
     itind=np.arange(0,L//nd)
     
-    psismaller=Psi[:,np.arange(0,L,2)]
+    psismaller=Psi[:,np.arange(0,L,nd)]
     
     func = partial(correlate,p,psismaller)
     #OR IF WE WANT TO BE VERY COOL   
@@ -304,10 +304,10 @@ def getp2(gs):
 def getPxxyyParr(p2):
 
     #nd=2 #simplifies calculation by using fewer visualizing points
-    M=np.zeros((L//nd,L//nd))
-    itind=np.arange(0,L//nd)
+    M=np.zeros((L//nd, L//nd))
+    itind=np.arange(0 ,L//nd)[::-1]
 
-    psismaller=Psi[:,np.arange(0,L,2)]
+    psismaller=Psi[:,np.arange(0,L,nd)]
     func = partial(correlate2,p2,psismaller)
     #OR IF WE WANT TO BE VERY COOL   
     pool = Pool(processes=16)
@@ -315,7 +315,7 @@ def getPxxyyParr(p2):
     pool.close()
     pool.join()
     result=result_Corr.get(timeout=120)
-    M += np.array(result)  
+    M += np.array(result)
     return(M)
 
 #Kernel for doing correlations, point*all other points ->vector    
@@ -332,6 +332,12 @@ def correlate2(p2,psi,it):
 
 
 
+def getS(p):
+    S=0
+    for i in range(0,K):
+        if p[i,i]>0:
+           S-=p[i,i]*np.log(p[i,i])  
+    return(S)
 
 
 
@@ -339,28 +345,108 @@ def correlate2(p2,psi,it):
 
 
 
+def FiniteT(beta,Evals,Evecs):
+    E0=Evals[0]
+    Z=np.sum(np.exp(-beta*(E0-Evals[:])))
+    
+    pz=getpz(Evecs,Evals,beta)/Z
+    Pzxy=getPxyParr(pz)
+    
+    
+    
+    
+    
+    return(pz,Pzxy,Z)
+
+def getpz(Evec,E,beta):
+    #global Psi
+    pz=np.zeros((K,K)) 
+    E0=E[0]
+  
+    for n in range(0,len(E)):
+        state_n=Evec[n]
+        for k in range(0,Fsize):
+            coeff=state_n[k]
+            for j in range(0,K):
+                if basis[k,j]==0:
+                    continue
+                else:
+                    for i in range(0,K):
+                        new=(basis[k]+0.0)
+                        A=np.sqrt(new[j])
+                        new[j]-=1
+                        new[i]+=1
+                        A*=np.sqrt(new[i])
+                    
+                        ind=searchBasis(new)
+                    
+                        pz[i,j]+=state_n[ind]*coeff*A*np.exp(-beta*(E[n]-E0))
+                    
+    return(pz)
+
+def getplot(f,args):
+    plt.figure()
+    plt.imshow(f(args))
+    plt.show()
+    return()
     
 def main():
+    
+    
+    
+    
+    #=====================================================================
+    #=====================================================================
+    #=====================================================================
     #Input parameters
     global L,K,N,Fsize,xmax,ix,x,dx,basis,U,Psi,BinomialTable,nd
-   
     L=int(2**10)
-    K=int(4) #states
-    N=int(10) #particles bosons
+    K=int(10) #states
+    N=int(2) #particles bosons
     Fsize=int(comb(K-1+N,K-1)) #size of Fock Basis
-    xmax=int(10)
+    xmax=int(7)
+    NG=1
+    Neig=15
+    gmax=int(8)
+    gmin=4
+    allplots=False
+    Tplots=True
+    Spectrum=False
+    
+    
+    Nb=7
+    betamin=0.1
+    betamax=4
+    #Tplots=False
+    nd=4 #factor which we cut down for visualizing L*L corrs
+    #The scaling for each 2body corr is about 2^(exponent - 6) seconds
+    #where the exponent is the power in L-log2(nd)
+    #=====================================================================
+    #=====================================================================
+    #=====================================================================
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     xmin=int(-xmax)
     ix=np.arange(0,L)
     x=np.linspace(xmin,xmax,L)
-    nd=2 #factor which we cut down for visualizing L*L corrs
-    #The scaling for each 2body corr is about 2^(exponent - 6) seconds
-    #where the exponent is the power in L-log2(nd)
     
     
     xnd=np.linspace(xmin,xmax,L//nd)
-    
     dx=x[1]-x[0]
-    allplots=True
     
     
     
@@ -379,18 +465,17 @@ def main():
 
 
 
-    NG=9
-    Neig=3
-    gmax=int(12)
-    gmin=-gmax
+
     G=np.linspace(gmin,gmax,NG)
     #G=np.array([2])
     #NG=1
     global E,EV
+    S_1=np.zeros((NG))
     E=np.zeros((NG,Neig))
     EV=np.zeros((NG),dtype='object')
     for iii in range(0,NG):
-        print(iii, 'of ', NG )
+        print(iii, 'of ', NG-1 )
+        print('g=',G[iii])
         S=timer()
         H=createH(G[iii])
         print('     %.5f H build time'%(timer()-S))
@@ -403,25 +488,77 @@ def main():
     
     
     cmap = cm.get_cmap('viridis')
+    #cmap = vapeplot.cmap('vaporwave')
     E=E.T
-    fig=plt.figure()
-    for i in range(0,Neig):
-        plt.plot(G,E[i],label='En_%i'%i,color=cmap(i/Neig))
-    plt.plot(0,N/2,'mx')
-
-    #plt.savefig('/home/aleksczejdo/Dokumenty/FewBody/KatalogZdjęć/%iParticles/%iN_%iK__SpectrumBoson.png'%(N,N,K))
-    plt.show()
+    if Spectrum==True:
+        plt.figure()
+        for i in range(0,Neig):
+            plt.plot(G,E[i],label='En_%i'%i,color=cmap(i/Neig))
+        plt.plot(0,N/2,'mx')
+    
+        #plt.savefig('/home/aleksczejdo/Dokumenty/FewBody/Plots/%iParticles/%iN_%iK__SpectrumBoson.png'%(N,N,K))
+        plt.show()
+    
+    
+    
+    
+    
+    
+    if Tplots==True:
+        if len(G)>0:
+            ii=0
+            gc=gmin
+        else:
+            gc=G
+        
+        barr=np.linspace(betamin,betamax,Nb)
+        print('Finite T for %i of %i eigenvalues at g=%.4f'%(Neig,Fsize,gc))
+        pzarr=np.zeros((Nb,K,K))
+        Pzxyarr=np.zeros((Nb,L//nd,L//nd))
+        Zarr=np.zeros((Nb))
+        
+        plt.figure(figsize=(12,4))
+        for i in range(0,Nb):
+           print('Calculating %i of %i Pij and Pxy'%(i,Nb-1))
+           b=barr[i]
+           
+           pzarr[i],Pzxyarr[i],Zarr[i]=FiniteT(b,E,EV[ii].T)
+        
+        
+           ax1= plt.subplot(3,Nb,i+1)
+           ax1.imshow(pzarr[i],cmap=cmap)
+           ax1.set_title('pij B=%.3f'%(b))
+           
+           ax2= plt.subplot(3,Nb,i+Nb+1)
+           ax2.imshow(Pzxyarr[i],cmap=cmap)
+           ax2.set_title('Pxy')
+           
+           y=np.diag(Pzxyarr[i])
+           x=np.linspace(0,1,len(y))
+           ax3= plt.subplot(3,Nb,i+2*Nb+1)
+           ax3.plot(x,y)
+           ax3.set_title('nprof')
+        #plt.tight_layout()
+        plt.suptitle('%i N %i K %.3f G'%(N,K,G))
+        plt.show()
+     
+        
+        
+        
+        
     if allplots==True:
         for i in range(0,NG):
         
         
-            print('g= ', G[i],',    %i  of %i'%(i,NG))
+            print('g= ', G[i],',    %i  of %i'%(i,NG-1))
             
             #Get 1 particle Density matrix
             n1=getp(EV[i][:,0])
             #print(np.trace(n1),'traceP')
             p=n1*1.0
-            
+        
+            if G[i]>=0:
+                S_1[i]=getS(p/N)
             #1 particle density corr
             S=timer()
             Pxy=getPxyParr(p)
@@ -461,11 +598,11 @@ def main():
             plt.figure(figsize=(14,14))
             
             ax1= plt.subplot(3,2,1)
-            ax1.imshow(p)
+            ax1.imshow(p,cmap=cmap)
             plt.title('Pij')
             
             ax2= plt.subplot(3,2,2)
-            ax2.imshow(p2)
+            ax2.imshow(p2,cmap=cmap)
             plt.title('Pij,kl')
         
             
@@ -476,17 +613,25 @@ def main():
         
             
             ax4 = plt.subplot(3,2,3)
-            ax4.imshow(Pxy)    
+            ax4.imshow(Pxy,cmap=cmap)    
             plt.title('One Particle DM P(x,y)')
+            
         
             
             ax5 = plt.subplot(3,2,4)
-            ax5.imshow(Pxxyy)    
+            ax5.imshow(Pxxyy,cmap=cmap)    
             plt.title('Two Particle DM P(xx,yy)')
             
             plt.tight_layout()
             plt.suptitle('%i Particles %i Exctited States \n %.4f Coupling'%(N,K,G[i]))
-            #plt.savefig('/home/aleksczejdo/Dokumenty/FewBody/KatalogZdjęć/%iParticles/%iN_%iK_%.4fG_Boson.png'%(N,N,K,G[i]))
+            #plt.savefig('/home/aleksczejdo/Dokumenty/FewBody/Plots/%iParticles/%iN_%iK_%.4fG_Boson.png'%(N,N,K,G[i]))
+
+            
+        plt.figure()
+        plt.clf()
+        plt.plot(G,S_1)
+        plt.title('Entanglement Entropy 1 particle')
+        plt.show()
     return()
 main()
     
